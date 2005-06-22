5887220e1f0a3c44a2018e7a8d90a98d1ad5fd54
timf <timf@e2a50384-0d36-e646-c335-9dcfbe33cb62>
2005-06-22 17:53:16 +0000

    Issue 8 fixed : we're now using java.net.URI.getPath() rather than ad-hoc methods to determine what a URI contains (the problem was, that the ad-hoc approach didn't consider that a URL would have a %20 in the URI instead of a space)

--- src/filters/gui/src/org/jvnet/olt/filters/gui/DropFileExtractor.java
+++ src/filters/gui/src/org/jvnet/olt/filters/gui/DropFileExtractor.java
@@ -1,6 +1,6 @@
 
 /*
- * Copyright  2005 Sun Microsystems, Inc. 
+ * Copyright  2005 Sun Microsystems, Inc.
  * All rights reserved Use is subject to license terms.
  *
  */
@@ -24,6 +24,9 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -39,13 +42,13 @@ import java.util.logging.Logger;
  */
 public class DropFileExtractor {
     private static final Logger logger = Logger.getLogger(DropFileExtractor.class.getName());
-
+    
     /**
      * Creates a new instance of DropFileExtractor.
      */
     public DropFileExtractor() {
     }
-
+    
     /**
      * Exracts file names from a drop event.
      * @param dtde Drop event to extract the filenames from
@@ -53,13 +56,13 @@ public class DropFileExtractor {
      */
     public List extractFiles(java.awt.dnd.DropTargetDropEvent dtde) {
         Transferable trans = dtde.getTransferable();
-
+        
         List fileList = Collections.EMPTY_LIST;
-
+        
         try {
             DataFlavor[] flavors = dtde.getCurrentDataFlavors();
             DataFlavor flavor = DataFlavor.selectBestTextFlavor(flavors);
-
+            
             if (flavor == null) {
                 for (int i = 0; i < flavors.length; i++) {
                     if (trans.isDataFlavorSupported(flavors[i])) {
@@ -74,7 +77,7 @@ public class DropFileExtractor {
         } catch (java.io.IOException e) {
             logger.throwing(getClass().getName(), "extractFiles",e);
         }
-
+        
         return fileList;
     }
     
@@ -84,34 +87,39 @@ public class DropFileExtractor {
      * this code is a little complex. I try to return a list of File objects
      * from this method.
      * @return a list of java.io.File objects containing the files to be converted
-     */    
+     */
     private List processDrop(Transferable trans, DataFlavor flavor) throws IOException, UnsupportedFlavorException {
         List fileList = new ArrayList();
         String repClassStr = flavor.getDefaultRepresentationClassAsString();
         String mimeType = flavor.getMimeType();
         logger.finer(flavor.getHumanPresentableName() + " == " + repClassStr + " m " + mimeType);
-
+        
         Object o = null;
-
+        
         // check for simple strings first, this appears to be okay on Solaris/GNOME
         if (trans.isDataFlavorSupported(DataFlavor.stringFlavor)) {
             String s = (String)trans.getTransferData(DataFlavor.stringFlavor);
             logger.log(Level.CONFIG, "TransferData == " + s);
-
+            
             String[] filenames = s.split("\n");
-
+            
             for (int i = 0; i < filenames.length; i++) {
-                File f = new File(filenames[i].replaceAll("file://", "").trim());
-                fileList.add(f);
+                try {
+                    URI u = new URI(filenames[i]);
+                    File f = new File(u.getPath().trim());
+                    fileList.add(f);
+                } catch (URISyntaxException e){
+                    throw new IOException("Unknown URI syntax :"+filenames[i]);
+                }
             }
-
+            
             // cool we're done!
             return fileList;
         }
-
+        
         // otherwise, things are more complex.
         o = trans.getTransferData(flavor);
-
+        
         // We need to work out what type this object is.
         // Different platforms handle drag+drop differently, so this is a bit
         // of a hack.
@@ -126,52 +134,53 @@ public class DropFileExtractor {
         } else {
             logger.finer("o was null!");
         }
-
+        
         return fileList;
     }
-
+    
     /**
      * MacOSX specific extraction woodoo
-     * @param o 
-     * @throws java.io.IOException 
-     * @return 
+     * @param o
+     * @throws java.io.IOException
+     * @return
      */
     private List getMacOSXFileList(Object o) throws java.io.IOException {
         List fileList = new ArrayList();
-
+        
         // Mac OSX appears to use InputStreamReader
         // I can't work out how to handle multiple files yet
         Reader reader = (InputStreamReader)o;
         int i = 0;
         StringBuffer buf = new StringBuffer();
-
+        
         while ((i = reader.read()) != -1) {
             buf.append((char)i);
         }
 
-        // buf now contains something like :
-        // file://localhost/Users/timf/Desktop/file.html
-        // this is not ideal, but it'll do for now.
-        File f = new File(buf.toString().replaceAll("file://localhost", "").trim());
-        fileList.add(f);
-
+        try {
+            URI u = new URI(buf.toString());
+            File f = new File(u.getPath().trim());
+            fileList.add(f);
+        } catch (URISyntaxException e){
+            throw new IOException("Unknown URI syntax for Mac filename :"+buf.toString());
+        }
         return fileList;
     }
-
+    
     /**
      * Win32 specific extraction woodoo
      */
     private List getWin32FileList(Object o) {
         List fileList = new ArrayList();
-
+        
         // Windows appears to give us a list of files (even if there's
         // only one file being dropped
         List l = (List)o;
         Iterator i = l.iterator();
-
+        
         while (i.hasNext()) {
             Object content = i.next();
-
+            
             if (content instanceof java.io.File) {
                 File f = (File)content;
                 fileList.add(f);
@@ -179,7 +188,7 @@ public class DropFileExtractor {
                 logger.log(Level.SEVERE, "List contents of type " + content.getClass().toString() + " are unknown.");
             }
         }
-
+        
         return fileList;
     }
 }
