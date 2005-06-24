09e86ad55b94e9482f2602acdd7193951c73008b
timf <timf@e2a50384-0d36-e646-c335-9dcfbe33cb62>
2005-06-24 10:30:46 +0000

    Fixed wrapper script for testing the xliff subsegmenter

--- src/filters/xliffsubsegmenter/run.sh
+++ src/filters/xliffsubsegmenter/run.sh
@@ -3,10 +3,18 @@ LC_ALL=en_US.UTF-8
 export LC_ALL
 LANG=en_US.UTF-8
 export LANG
-CLASSPATH=.:/space/timf/transtech/utilities/iAS7/share/lib/jaxrpc-api.jar:/space/timf/transtech/utilities/jars/jaxb/namespace.jar://space/timf/transtech/utilities/jars/jaxb/xsdlib.jar:/space/timf/transtech/utilities/jars/jaxb//jaxb-libs.jar:/space/timf/transtech/utilities/jars/jaxb//jaxb-impl.jar://space/timf/transtech/utilities/jars/jaxb//jaxb-api.jar:build:/space/timf/transtech/build/filters.jar:/space/timf/transtech/build/tt-common.jar
+
+JAXB_LIBS=../../../utilities/jars/jwsdp-1.5/jaxb/lib/jaxb-api.jar:../../../utilities/jars/jwsdp-1.5/jaxb/lib/jaxb-impl.jar:../../../utilities/jars/jwsdp-1.5/jaxb/lib/jaxb-libs.jar
+
+JAXP_LIBS=../../../utilities/jars/jwsdp-1.5/jaxp/lib/jaxp-api.jar:../../../utilities/jars/jwsdp-1.5/jaxp/lib/endorsed/xercesImpl.jar:../../../utilities/jars/jwsdp-1.5/jaxp/lib/endorsed/dom.jar:../../../utilities/jars/jwsdp-1.5/jaxp/lib/endorsed/sax.jar:../../../utilities/jars/jwsdp-1.5/jaxp/lib/endorsed/xalan.jar
+
+JWSDP_COMMON=../../..//utilities/jars/jwsdp-1.5/jwsdp-shared/lib/jax-qname.jar:../../..//utilities/jars/jwsdp-1.5/jwsdp-shared/lib/namespace.jar:../../..//utilities/jars/jwsdp-1.5/jwsdp-shared/lib/relaxngDatatype.jar:../../..//utilities/jars/jwsdp-1.5/jwsdp-shared/lib/xsdlib.jar
+
+
+CLASSPATH=../../../build/filters.jar:../../../build/tt-common.jar:$JAXB_LIBS:$JAXP_LIBS:$JWSDP_COMMON
 export CLASSPATH
 xDEBUG="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=58000"
 
 
-$JAVA_HOME/bin/java -Xmx128m com.sun.tt.filters.xliffsubsegment.XliffSubSegmenter $@
+$JAVA_HOME/bin/java -Xmx128m org.jvnet.olt.filters.xliffsubsegment.XliffSubSegmenter $@
 
