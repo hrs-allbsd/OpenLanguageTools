50786e021c4537b9472bce265c1247148fb2b645
nobody <nobody@e2a50384-0d36-e646-c335-9dcfbe33cb62>
2005-07-19 19:23:04 +0000

    This commit was manufactured by cvs2svn to create branch 'EDITOR_1_1_0'.

--- src/filters/gui/installer/filters.bat
+++
@@ -1,5 +0,0 @@
-SET OLT_HOME="$INSTALL_PATH"
-
-CD "%OLT_HOME"
-
-java -Xmx256M  -classpath resources;classes\filters.jar;classes\tt-common.jar;classes\dtdparser121.jar;classes\xerces2.jar;classes\xmlParserAPIs.jar org.jvnet.olt.filters.gui.XliffFilterGUI "$INSTALL_PATH\resources"
