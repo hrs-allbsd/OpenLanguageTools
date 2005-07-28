5b4839dd197a133fb045fc32a7d49e7f1853fb85
timf <timf@e2a50384-0d36-e646-c335-9dcfbe33cb62>
2005-07-28 08:25:19 +0000

    Fixing the windows batch file for the filters Issue number:  50

--- src/filters/gui/installer/filters.bat
+++ src/filters/gui/installer/filters.bat
@@ -1,5 +1,5 @@
 SET OLT_HOME="$INSTALL_PATH"
 
-CD "%OLT_HOME"
+CD "%OLT_HOME%"
 
 java -Xmx256M  -classpath resources;classes\filters.jar;classes\tt-common.jar;classes\dtdparser121.jar;classes\xerces2.jar;classes\xmlParserAPIs.jar org.jvnet.olt.filters.gui.XliffFilterGUI "$INSTALL_PATH\resources"
