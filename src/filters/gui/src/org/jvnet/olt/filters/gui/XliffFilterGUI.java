bcc08a9bde50ad2021d79067022d1ac4c2a2da55
nobody <nobody@e2a50384-0d36-e646-c335-9dcfbe33cb62>
2005-09-16 13:59:25 +0000

    This commit was manufactured by cvs2svn to create branch 'FILTERS_1_2_0'.

--- src/filters/gui/src/org/jvnet/olt/filters/gui/XliffFilterGUI.java
+++ src/filters/gui/src/org/jvnet/olt/filters/gui/XliffFilterGUI.java
@@ -26,18 +26,14 @@ import java.io.FileInputStream;
 import java.text.MessageFormat;
 import java.util.Enumeration;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
-import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.prefs.Preferences;
 import javax.swing.JOptionPane;
-import javax.swing.JTable;
 import javax.swing.SwingUtilities;
-import javax.swing.table.TableModel;
 
 /**
  * Simple GUI code to do drag & drop XLIFF Conversion. Tested and works on
@@ -50,40 +46,25 @@ import javax.swing.table.TableModel;
 public class XliffFilterGUI extends javax.swing.JFrame implements DropTargetListener {
     
     private DropFileExtractor extr =  new DropFileExtractor();
-    private boolean firstShowConfig = true;
-    private boolean firstShowLog = true;
+    private boolean firstShow = true;
     private Map attributes = null;
-    // underlying preferences store for the attributes
-    // currently, these need to be synchronised manually
-    // by the programmer when changing attribute values - see setAttribute(Object key, Object value)
-    private Preferences prefs = null;
-    
-    // for testing
-    // private Map attributes = new HashMap();
-    // {
-    //   attributes.put("xxx", "yyyy");
-    //   attributes.put("Hello", "Dolly");
-    // }
     private String resourcesDir = null;
     
-    //height of log when first 'Show Log'/'Show Config' button hit first time
+    //height of log when first 'Show Log' button hit first time
     private int PREFFERED_HEIGHT = 200;
     
     /** Creates new form XliffFilterGUI */
-    public XliffFilterGUI(String resourcesDir) {
-        // for convenience, we just use the default logger.
-        logger = Logger.getLogger("Conversion");
-        
-        this.resourcesDir = resourcesDir;
-        this.attributes = getUserPreferences();
+    public XliffFilterGUI() {
         initComponents();
-        //add textarea log handler
-        logger.addHandler(new TextAreaHandler(logTextArea));
-        
         // make a drop target that uses this as it's lister
         DropTarget d = new DropTarget((Component)jLabel1, (DropTargetListener)this);
         d.setActive(true);
         
+        // for convenience, we just use the default logger.
+        logger = Logger.getLogger("Conversion");
+        
+        //add textarea log handler
+        logger.addHandler(new TextAreaHandler(logTextArea));
         
     }
     
@@ -97,67 +78,18 @@ public class XliffFilterGUI extends javax.swing.JFrame implements DropTargetList
     private void initComponents() {
         java.awt.GridBagConstraints gridBagConstraints;
 
+        showLogBtn = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         jScrollPane1.setVisible(false);
         setShowLogBtnText();
         logTextArea = new javax.swing.JTextArea();
         jLabel1 = new javax.swing.JLabel();
-        showConfigBtn = new javax.swing.JButton();
-        showLogBtn = new javax.swing.JButton();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        jScrollPane2.setVisible(false);
-        setShowConfigBtnText();
-
-        jTable1 = new javax.swing.JTable();
 
         getContentPane().setLayout(new java.awt.GridBagLayout());
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Open Language Tools XLIFF Filters");
-        jScrollPane1.setDoubleBuffered(true);
-        jScrollPane1.setMinimumSize(new java.awt.Dimension(200, 100));
-        jScrollPane1.setPreferredSize(new java.awt.Dimension(150, 200));
-        logTextArea.setEditable(false);
-        logTextArea.setLineWrap(true);
-        logTextArea.setDisabledTextColor(new java.awt.Color(51, 51, 51));
-        jScrollPane1.setViewportView(logTextArea);
-
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 2;
-        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
-        gridBagConstraints.weightx = 0.5;
-        gridBagConstraints.weighty = 0.5;
-        getContentPane().add(jScrollPane1, gridBagConstraints);
-
-        jLabel1.setFont(new java.awt.Font("Dialog", 1, 36));
-        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText(xliffFilterGUIMessages.getString("Drop_files_here"));
-        jLabel1.setBorder(new javax.swing.border.TitledBorder("Open Language Tools XLIFF Filters"));
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 0;
-        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
-        gridBagConstraints.weightx = 1.0;
-        gridBagConstraints.insets = new java.awt.Insets(5, 5, 15, 5);
-        getContentPane().add(jLabel1, gridBagConstraints);
-
-        showConfigBtn.setText(java.util.ResourceBundle.getBundle("org/jvnet/olt/filters/gui/XliffFilterGUIMessages").getString("Show_Config_>>>"));
-        showConfigBtn.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                showConfigBtnActionPerformed(evt);
-            }
-        });
-
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 1;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.EAST;
-        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
-        getContentPane().add(showConfigBtn, gridBagConstraints);
-
         showLogBtn.setText(xliffFilterGUIMessages.getString("Show_log_>>>"));
-        setShowConfigBtnText();
         showLogBtn.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 showLogBtnActionPerformed(evt);
@@ -171,42 +103,33 @@ public class XliffFilterGUI extends javax.swing.JFrame implements DropTargetList
         gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
         getContentPane().add(showLogBtn, gridBagConstraints);
 
-        jScrollPane2.setPreferredSize(new java.awt.Dimension(150, 200));
-        jTable1.setModel(getTableModel());
-        jTable1.setPreferredSize(new java.awt.Dimension(200, 150));
-        jScrollPane2.setViewportView(jTable1);
+        jScrollPane1.setPreferredSize(new java.awt.Dimension(150, 200));
+        logTextArea.setEditable(false);
+        logTextArea.setLineWrap(true);
+        logTextArea.setDisabledTextColor(new java.awt.Color(51, 51, 51));
+        jScrollPane1.setViewportView(logTextArea);
 
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 3;
+        gridBagConstraints.gridy = 2;
         gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
-        gridBagConstraints.weightx = 0.5;
-        gridBagConstraints.weighty = 0.5;
-        getContentPane().add(jScrollPane2, gridBagConstraints);
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.weighty = 1.0;
+        getContentPane().add(jScrollPane1, gridBagConstraints);
+
+        jLabel1.setFont(new java.awt.Font("Dialog", 1, 36));
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText(xliffFilterGUIMessages.getString("Drop_files_here"));
+        jLabel1.setBorder(new javax.swing.border.TitledBorder("Open Language Tools XLIFF Filters"));
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 15, 5);
+        getContentPane().add(jLabel1, gridBagConstraints);
 
         pack();
     }
     // </editor-fold>//GEN-END:initComponents
-    
-    private void showConfigBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_showConfigBtnActionPerformed
-// TODO add your handling code here:
-        //Show / hide config button action
-        boolean newState = !jScrollPane2.isVisible();
-        jScrollPane2.setVisible(! jScrollPane2.isVisible());
-        int height = (int)jScrollPane2.getSize().getHeight();
-        if(firstShowConfig){
-            height = PREFFERED_HEIGHT;
-            firstShowConfig = false;
-        }
-        
-        Dimension d = getSize();
-        d.setSize(d.getWidth(),d.getHeight() + (newState ? 1 : -1) * height);
-        setSize(d);
-        setShowConfigBtnText();
-        updateLableLayout();
-        repaint();
-        pack();
-    }//GEN-LAST:event_showConfigBtnActionPerformed
     
     private void showLogBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_showLogBtnActionPerformed
         
@@ -215,9 +138,9 @@ public class XliffFilterGUI extends javax.swing.JFrame implements DropTargetList
         boolean newState = !jScrollPane1.isVisible();
         jScrollPane1.setVisible(! jScrollPane1.isVisible());
         int height = (int)jScrollPane1.getSize().getHeight();
-        if(firstShowLog){
+        if(firstShow){
             height = PREFFERED_HEIGHT;
-            firstShowLog = false;
+            firstShow = false;
         }
         
         Dimension d = getSize();
@@ -237,7 +160,8 @@ public class XliffFilterGUI extends javax.swing.JFrame implements DropTargetList
         final String resourcesDir = args[0];
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                XliffFilterGUI gui = new XliffFilterGUI(resourcesDir);
+                XliffFilterGUI gui = new XliffFilterGUI();
+                gui.setResourcesDir(resourcesDir);
                 gui.setVisible(true);
             }
         });
@@ -352,11 +276,6 @@ public class XliffFilterGUI extends javax.swing.JFrame implements DropTargetList
             showLogBtn.setText(jScrollPane1.isVisible() ? xliffFilterGUIMessages.getString("_Hide_log_<<<") : xliffFilterGUIMessages.getString("Show_log_>>>"));
     }
     
-    private void setShowConfigBtnText(){
-        if(showConfigBtn != null && jScrollPane2 != null)
-            showConfigBtn.setText(jScrollPane2.isVisible() ? xliffFilterGUIMessages.getString("Hide_Config_<<<") : xliffFilterGUIMessages.getString("Show_Config_>>>"));
-    }
-    
     private void updateLableLayout(){
         boolean visible = jScrollPane1.isVisible();
         
@@ -367,17 +286,6 @@ public class XliffFilterGUI extends javax.swing.JFrame implements DropTargetList
         layout.setConstraints(jLabel1,c);
     }
     
-    
-    private TableModel getTableModel(){
-        if (this.attributes != null){
-            UserPrefsTableModel model = new UserPrefsTableModel(this.attributes, this);
-            return model;
-        } else {
-            this.logger.log(Level.SEVERE, "WARNING, trying to set table model when attributes are empty!");
-            return null;
-        }
-    }
-    
     /**
      * Here's where we need to set/configure user preferences : we return a map
      * of key/value pairs that we can use to configure the filters.
@@ -386,7 +294,7 @@ public class XliffFilterGUI extends javax.swing.JFrame implements DropTargetList
     private Map getUserPreferences(){
         
         Map attribs = new HashMap();
-        this.prefs = Preferences.userNodeForPackage(XliffFilterGUI.class);
+        Preferences prefs = Preferences.userNodeForPackage(XliffFilterGUI.class);
         attribs.put("source.language",
                 prefs.get("source.language", "en-US"));
         attribs.put("file.encoding",
@@ -407,17 +315,15 @@ public class XliffFilterGUI extends javax.swing.JFrame implements DropTargetList
         //boolean configuredXmlStore = prefs.getBoolean("configured.xml.config", false);
         
         //if (!configuredXmlStore){
-/*
-        try {
-            XliffFilterFacade.updateXmlConfigRepository(resourcesDir+File.separator+"xml-config",
-                    resourcesDir+File.separator+".xml-config-store",
-                    resourcesDir+File.separator+"xml-config.dtd");
- 
-            prefs.putBoolean("configured.xml.config", true);
-        } catch (XliffFilterFacadeException e){
-            this.logger.log(Level.SEVERE,xliffFilterGUIMessages.getString("Problem_configuring_XML_filter!"),e);
-        }
- */
+            try {
+                XliffFilterFacade.updateXmlConfigRepository(resourcesDir+File.separator+"xml-config",
+                        resourcesDir+File.separator+".xml-config-store",
+                        resourcesDir+File.separator+"xml-config.dtd");
+                
+                prefs.putBoolean("configured.xml.config", true);
+            } catch (XliffFilterFacadeException e){
+                this.logger.log(Level.SEVERE,xliffFilterGUIMessages.getString("Problem_configuring_XML_filter!"),e);
+            }
         //}
         return attribs;
     }
@@ -434,27 +340,15 @@ public class XliffFilterGUI extends javax.swing.JFrame implements DropTargetList
         this.attributes = getUserPreferences();
     }
     
-    /**
-     * used to set key/value pairs that are passed into the filters
-     * conversion proper
-     */
-    protected void setAttribute(Object key, Object value){
-        this.attributes.put(key, value);
-        this.prefs.put((String)key, (String)value);
-    }
-    
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTable jTable1;
     private javax.swing.JTextArea logTextArea;
-    private javax.swing.JButton showConfigBtn;
     private javax.swing.JButton showLogBtn;
     // End of variables declaration//GEN-END:variables
     /** A logger we can use to capture logging output with
      */
     private Logger logger = null;
-    
+
     private static final java.util.ResourceBundle xliffFilterGUIMessages = java.util.ResourceBundle.getBundle("org/jvnet/olt/filters/gui/XliffFilterGUIMessages");
 }
