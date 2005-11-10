2f719e3d5f2beaec706abd62893f86e7837f86f9
boris_steiner <boris_steiner@e2a50384-0d36-e646-c335-9dcfbe33cb62>
2005-11-10 08:50:40 +0000

    Fixed tag update algorithm to work as expected.
    The new algorithm tries to find the best approximation of tags between source
    and new target, so that the new target will contain all tags that are in source.
    Redundant tags will be removed from target, extra ones appended to the end etc.
    
    
    Issue number:  104
    Obtained from:
    Submitted by:
    Reviewed by:

--- src/transeditor/src/org/jvnet/olt/editor/model/SimpleSentenceHelper.java
+++ src/transeditor/src/org/jvnet/olt/editor/model/SimpleSentenceHelper.java
@@ -23,8 +23,44 @@ import java.util.logging.Logger;
  */
 public class SimpleSentenceHelper {
     private static final Logger logger = Logger.getLogger(SimpleSentenceHelper.class.getName());
+
+    /** Holds element and its contents when it's a tag
+     *  element is text between tags or tags
+     *  tag is parsed tag and attributes
+     *
+     */
+    static class TagHolder {
+	public final PivotBaseElement element;
+	public final ContentTag tag;
+	
+	
+	TagHolder(PivotBaseElement element,ContentTag tag){
+	    if(element == null || tag == null)
+		throw new NullPointerException();
+	    
+	    this.element = element;
+	    this.tag = tag;
+	}
+	
+	
+	public int hashCode() {
+	    return element.hashCode()*37+tag.hashCode()*11;
+	}
+	
+	public boolean equals(Object obj) {
+	    if(obj instanceof TagHolder){
+		TagHolder other = (TagHolder)obj;
+		
+		//This is based on identity because this code deserves it!
+		return other.element == element && other.tag == tag;
+	    }
+	    
+	    return false;
+	}
+	
+	
+    }
     
- 
     /** Creates a new instance of SimpleSentenceHelper */
     public SimpleSentenceHelper() {
     }
@@ -34,210 +70,106 @@ public class SimpleSentenceHelper {
     }
     
     static public  String formatTranslation(String s, String matchSrc, String matchT,boolean tagUpdate) {
-	PivotText newSrc = null;
-	PivotText oldSrc = null;
-	PivotText oldTarget = null;
+	if(s == null || matchSrc == null || matchT == null)
+	    throw new NullPointerException();
 	
 	if(!tagUpdate || s.equals(matchT))
 	    return matchT;
 	
-	try {
-	    //newsrc diff from oldsrc
-	    newSrc = new PivotText(s, true);
-	    oldSrc = new PivotText(matchSrc, true);
-	    oldTarget = new PivotText(matchT, true);
+	PivotText newSrc = new PivotText(s, true);
+	PivotText oldSrc = new PivotText(matchSrc, true);
+	PivotText oldTarget = new PivotText(matchT, true);
+	
+	List srcTags = buildTagHolders(newSrc);
+	
+	List tgtTags = buildTagHolders(oldTarget);
+	
+	List tagsToKeep = new LinkedList();
+	
+	//1: find tags from source in  the target
+	for (Iterator i = srcTags.iterator(); i.hasNext();) {
+	    TagHolder srcTag = (TagHolder) i.next();
 	    
-	    
-	    
-	    
-	    if (newSrc.ct.length != 0) { //newSrc has tags.
+	    for (Iterator j = tgtTags.iterator(); j.hasNext();) {
+		TagHolder tgtTag = (TagHolder)j.next();
 		
-		if (oldSrc.ct.length != 0) { //oldsrc has tags
-		    
-		    if (oldTarget.ct.length != 0) { //oldtarget has tags
-			
-			Vector vTemp = compareTags(newSrc.ct, oldSrc.ct);
-			String xx = vTemp.toString();
-			logger.finest("result1:"+xx);
-			Vector vTempLocal = compareTags((ContentTag[])vTemp.toArray(new ContentTag[0]), oldTarget.ct);
-			
-			xx = vTempLocal.toString();
-			logger.finest("result2:"+xx);
-			
-			String deletedTag = oldTarget.deleteTag();
-			
-			return constructReturnString((ContentTag[])vTempLocal.toArray(new ContentTag[0]), deletedTag);
-			
-		    } else { //oldtarget has no tags
-			
-			Vector vTemp = compareTags(newSrc.ct, oldSrc.ct);
-			
-			return constructReturnString((ContentTag[])vTemp.toArray(new ContentTag[0]), matchT);
-		    }
-		} else { //oldsrc has no tags
-		    
-		    if (oldTarget.ct.length != 0) { //oldtarget has tags
-			
-			Vector vTemp = compareTags(newSrc.ct, oldTarget.ct);
-			
-			return constructReturnString((ContentTag[])vTemp.toArray(new ContentTag[0]), matchT);
-		    } else { //oldtarget has no tags
-			
-			return constructReturnString(newSrc.ct, matchT);
-		    }
-		}
-	    } else { //newSrc has no tags
-		
-		if (oldSrc.ct.length != 0) { //oldsrc has tags
-		    
-		    if (oldTarget.ct.length != 0) { //oldtarget has tags
-			
-			return oldTarget.deleteTag();
-		    } else { //oldtarget has no tags
-			
-			return matchT;
-		    }
-		} else { //oldsrc has no tags
-		    
-		    if (oldTarget.ct.length != 0) { //oldtarget has tags
-			
-			return oldTarget.deleteTag();
-		    } else { //oldtarget has no tags
-			
-			return matchT;
-		    }
-		}
-	    }
-	} catch (Exception ec) {
-	    logger.severe("Got exception:"+ec);
-	    //throw new RuntimeException(ec);
-	} finally {
-	    newSrc = null;
-	    oldSrc = null;
-	    oldTarget = null;
-	}
-	
-	return null;
-    }
-    
-    static String constructReturnString(ContentTag[] oInput, String strMatchT) {
-	String strRet = "";
-	
-	if (oInput.length >= 2) {
-	    if ((oInput[0]).iPos == 0) {
-		if ((oInput[0]).bFlag) {
-		    strRet += (oInput[0].strText + strMatchT);
-		} else {
-		    strRet += (oInput[0].strText + strMatchT + "</" + oInput[0].strTagName + ">");
-		}
-	    } else {
-		strRet += strMatchT;
-		
-		if (oInput[0].bFlag) {
-		    strRet += oInput[0].strText;
-		} else {
-		    strRet += (oInput[0].strText + "</" + oInput[0].strTagName + ">");
-		}
-	    }
-	    
-	    for (int i = 1; i < oInput.length; i++) {
-		if (oInput[i].bFlag) {
-		    strRet += oInput[i].strText;
-		} else {
-		    strRet += (oInput[i].strText + "</" + oInput[i].strTagName + ">");
-		}
-	    }
-	} else if (oInput.length == 1) {
-	    if (oInput[0].iPos == 0) {
-		if (oInput[0].bFlag) {
-		    strRet += (oInput[0].strText + strMatchT);
-		} else {
-		    strRet += (oInput[0].strText + strMatchT + "</" + oInput[0].strTagName + ">");
-		}
-	    } else {
-		strRet += strMatchT;
-		
-		if (oInput[0].bFlag) {
-		    strRet += oInput[0].strText;
-		} else {
-		    strRet += (oInput[0].strText + "</" + (oInput[0]).strTagName + ">");
-		}
-	    }
-	}
-	
-	return strRet;
-    }
-    
-    
-    static Vector compareTags(ContentTag[] src, ContentTag[] target) {
-	Vector v = new Vector();
-	Vector vSrc = new Vector();
-	Vector vTarget = new Vector();
-	
-	
-	for (int i = 0; i < src.length; i++) {
-	    vSrc.add(src[i]);
-	}
-	
-	for (int i = 0; i < target.length; i++) {
-	    vTarget.add(target[i]);
-	}
-	
-	for (int i = 0; i < vSrc.size(); i++) {
-	    for (int j = 0; j < vTarget.size(); j++) {
-		int iResult = ((ContentTag)vSrc.elementAt(i)).compare((ContentTag)vTarget.elementAt(j));
-		
-		if (iResult == 0) { //complete same, we will use target;
-		    v.addElement(vTarget.elementAt(j));
-		    vSrc.set(i, null);
-		    vTarget.set(j, null);
-		    
+		//if a source tag is found in target mark it to keep
+		//remove from source tags so that at the end we have all tags
+		// from source that need to be added to target
+		// remove from target so that only those to remove are left
+		if(srcTag.tag.compare(tgtTag.tag) == ContentTag.COMPARE_TAGS_EQUAL){
+		    tagsToKeep.add(tgtTag);
+		    i.remove();
+		    j.remove();
 		    break;
-		} else if (iResult == 1) { //tag name same, attribute diff
-		    
-		    int iTemp = j;
-		    
-		    for (int k = j + 1; k < vTarget.size(); k++) {
-			int iResultTemp = ((ContentTag)vSrc.elementAt(i)).compare((ContentTag)vTarget.elementAt(k));
-			
-			if (iResultTemp != 0) {
-			    continue;
-			} else {
-			    iTemp = k;
-			    
-			    break;
-			}
-		    }
-		    
-		    if (iTemp > j) {
-			v.addElement((ContentTag)vTarget.elementAt(iTemp));
-			vSrc.set(i, null);
-			vTarget.set(iTemp, null);
-			
-			break;
-		    } else {
-			v.addElement((ContentTag)vSrc.elementAt(i));
-			vSrc.set(i, null);
-			
-			break;
-		    }
-		} else { //quit different
-		    
-		    continue;
 		}
+		
 	    }
 	}
 	
-	if (vSrc.size() != 0) {
-	    for (int i = 0; i < vSrc.size(); i++) {
-		if (vSrc.elementAt(i) != null) {
-		    v.addElement(vSrc.elementAt(i));
-		    vSrc.set(i, null);
-		}
-	    }
-	}
+	List pivots = new LinkedList(Arrays.asList(oldTarget.elements()));
 	
-	return v;
+	removeRedundantTags(pivots,tgtTags);
+	addExtraTags(pivots,srcTags);
+	
+	String result = buildResult(pivots);
+	
+	return result;
     }
-   
+    
+    /**
+     * modifies pivots !
+     */
+    static void removeRedundantTags(List pivots,List targetTags){
+	for (Iterator i = targetTags.iterator(); i.hasNext();) {
+	    TagHolder hldr = (TagHolder) i.next();
+	    
+	    pivots.remove(hldr.element);
+	    
+	}
+    }
+    
+    static void addExtraTags(List pivots, List extraTags) {
+	for (Iterator i = extraTags.iterator(); i.hasNext();) {
+	    TagHolder hldr = (TagHolder) i.next();
+	    pivots.add(hldr.element);
+	}
+    }
+    
+    static String buildResult(List pivots){
+	StringBuffer sb = new StringBuffer();
+	for (Iterator i = pivots.iterator(); i.hasNext();) {
+	    PivotBaseElement elem = (PivotBaseElement) i.next();
+	    
+	    sb.append(elem.getContent());
+	}
+	return sb.toString();
+    }
+    
+    static List buildTagHolders(PivotText pivot){
+	List rv = new LinkedList();
+	
+	ContentTag[] ctags = pivot.ct;
+	
+	if(ctags.length == 0)
+	    return Collections.EMPTY_LIST;
+	
+	PivotBaseElement[] elems = pivot.elements();
+	
+	int offset = 0;
+	int j = 0;
+	for (int i = 0; i < elems.length; i++) {
+	    if(! (elems[i].getElemType() == PivotBaseElement.TEXT)){
+		if(offset == ctags[j].iPos){
+		    rv.add(new TagHolder(elems[i],ctags[j]));
+		    //stop condition; we used all content tags so just bail out
+		    if(++j >= ctags.length)
+			break;
+		}
+	    }
+	    offset += elems[i].getVisibleLength();
+	}
+	
+	return rv;
+    }
 }
