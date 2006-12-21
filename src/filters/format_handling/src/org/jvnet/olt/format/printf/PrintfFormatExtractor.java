9a4836d62efe1a285c6de25e74c6894c73dd1718
boris_steiner <boris_steiner@e2a50384-0d36-e646-c335-9dcfbe33cb62>
2006-12-21 19:54:15 +0000

    Merge from HEAD Reimplementation of format extractors

--- src/filters/format_handling/src/org/jvnet/olt/format/printf/PrintfFormatExtractor.java
+++ src/filters/format_handling/src/org/jvnet/olt/format/printf/PrintfFormatExtractor.java
@@ -1,6 +1,6 @@
 
 /*
- * Copyright  2005 Sun Microsystems, Inc. 
+ * Copyright  2005 Sun Microsystems, Inc.
  * All rights reserved Use is subject to license terms.
  *
  */
@@ -12,13 +12,19 @@
  */
 
 package org.jvnet.olt.format.printf;
+
 import java.util.HashMap;
+import java.util.Map;
 import java.io.StringWriter;
 import java.io.IOException;
 import java.io.StringReader;
 import org.jvnet.olt.format.GlobalVariableManager;
+import org.jvnet.olt.format.GlobalVariableManagerException;
 import org.jvnet.olt.parsers.PrintfParser.*;
 import org.jvnet.olt.format.InvalidFormattingException;
+import org.jvnet.olt.parsers.SgmlDocFragmentParser.SgmlDocFragmentParserTreeConstants;
+import org.jvnet.olt.format.FormatItem;
+import org.jvnet.olt.format.AbstractFormatExtractionVisitor;
 
 /**
  *
@@ -30,7 +36,7 @@ public class PrintfFormatExtractor implements org.jvnet.olt.format.FormatExtract
     public PrintfFormatExtractor() {
     }
     
-    /** 
+    /**
      * This method returns a list of the formatting codes that were found in the
      *  string presented for formatting.
      */
@@ -38,16 +44,115 @@ public class PrintfFormatExtractor implements org.jvnet.olt.format.FormatExtract
         HashMap formats = new HashMap();
         try {
             StringReader reader = new StringReader(string);
-            PrintfParser parser = new PrintfParser(reader); 
+            PrintfParser parser = new PrintfParser(reader);
             parser.parse();
-            PrintfFormatExtractionVisitor visitor = new PrintfFormatExtractionVisitor();            
+            PrintfFormatExtractionVisitor visitor = new PrintfFormatExtractionVisitor();
             parser.walkParseTree(visitor, null);
             formats = visitor.getFormatItems();
         } catch (Throwable e){
             throw new InvalidFormattingException("Invalid printf sequence while trying to extract formatting from " +string);
             
-        }      
+        }
         return formats;
     }
+    
+    /**
+     * Convert formatted segment to normalized form that can be used by string comparer algorithm.
+     *
+     * The tags are converted to unique charactares with the formatMap. The text is converted to character 'X'.
+     *
+     * For example the formatted segment: "This is a &lt;b&gt;test&lt;b&gt;." is converted to "XAXB"
+     *
+     * @param formattedSegment the segment with text and formatting
+     *
+     * @param formatMap a map that contains mapping from a tag to unique character.
+     *        For example: '&lt;b&gt;' -- 'A'.
+     *        The tags in formattedSegmented are replace with the unique characters. If the tag is currently not
+     *        in the map. A new entry with additionally unique character is added.
+     *
+     * @param gvm the global variable manager that is used by specific file type parser.
+     *
+     * @return normalized String.
+     *
+     * @return InvalidFormattingException if the formatting cannot be parsed with the parser
+     */
+    public String getNormalizedForm(String formattedSegment,Map formatMap, GlobalVariableManager gvm) throws InvalidFormattingException {
+        String normalizedForm = "";
+        
+        try {
+            StringReader reader = new StringReader(formattedSegment);
+            PrintfParser parser = new PrintfParser(reader);
+            parser.parse();
+            
+            FormatExtractionVisitor visitor = new FormatExtractionVisitor(formatMap);
+            parser.walkParseTree(visitor, null);
+            
+            normalizedForm = visitor.getNormalizedForm();
+        } catch(Exception exParse) {
+            exParse.printStackTrace();
+            throw new InvalidFormattingException(exParse.getMessage());
+        }
+        
+        return normalizedForm;
+    }
+    
+    /** This class defines a visitor that traverses the parse tree of a
+     *  string containing printf-style formatting, and returns a map of
+     *  formatting based on the number of occurrences of each formatting
+     *  chunk ("%s", "%d", "%23f" would be three such chunks) - the key
+     *  is the chunk of formatting, the value is the number of times it
+     *  appears. It also evaluates the "severity" of different formatting
+     *  types. We apply a greater penalty to %d, %s and their type than we
+     *  do to \t \b \n.
+     */
+    public class FormatExtractionVisitor extends AbstractFormatExtractionVisitor implements PrintfParserVisitor, PrintfParserTreeConstants, SgmlDocFragmentParserTreeConstants {
+        
+        /** Constructs a new PrintfFormatExtractingVisitor.
+         */
+        public FormatExtractionVisitor(Map formatMap) {
+            super(formatMap);
+        }
+        
+        /** This method visits a node in the parse tree of an printf document
+         * fragment and processes it to see if it represents formatting. If
+         * it does represent formatting then it is added to a collection.
+         * @param node The node being visited
+         * @param data Ancillary data to aid in the processing task.
+         * @return A modified version (perhaps) of the data object.
+         */
+        public Object visit(SimpleNode node, Object data) {
+            int type = node.getType();
+            String nodeText = node.getNodeData();
+            handleNodes(type, nodeText);
+            return data;
+        }
+        
+        
+        /**
+         * @param type
+         * @param nodeText
+         * @param node  */
+        protected void handleNodes(int type, String nodeText) {
+            switch (type){
+                
+                case JJTCONV:
+                case JJTVARIABLECONV:
+                    addFormat(nodeText);
+                    break;
+                case JJTSLASHESCAPE:
+                case JJTOTHERESCAPE:
+                    addFormat(nodeText);
+                    break;
+                case JJTWORDS:
+                case JJTPCDATA:
+                    addPCData();
+                    break;
+            }
+            
+        }
+        
+        
+    }
+    
     
 }
