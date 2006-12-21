c93c7e8142287f235a8e33801dc2476727a026eb
boris_steiner <boris_steiner@e2a50384-0d36-e646-c335-9dcfbe33cb62>
2006-12-21 19:02:57 +0000

    Made UNIX script relocable; added dependecy to swing-layout-1.0.1.jar in the .bat file

--- src/transeditor/installer/translation.sh
+++ src/transeditor/installer/translation.sh
@@ -7,48 +7,48 @@
 MEMORY=256
 #MEMORY=512
 
+ED_INSTALL_PATH=$INSTALL_PATH
+ED_JAVA_HOME=$JAVA_HOME
+LOGGING=-Djava.util.logging.config.file=logging.properties
+xDEBUG="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=58000"
 
-if [ -d "$INSTALL_PATH" ] ; then
-	SUNTRANS_HOME="$INSTALL_PATH"
-	export SUNTRANS_HOME
-else
-	echo "The installation directory does not seem to exist: $INSTALL_PATH"
-	echo "Please re-install the application"
-	exit 1	
+
+SUNTRANS_HOME="${ED_INSTALL_PATH}"
+if [ ! -d "${ED_INSTALL_PATH}" ] ; then
+	SUNTRANS_HOME=`dirname $0`;
 fi
 
-cd "$INSTALL_PATH"
+export SUNTRANS_HOME
+
+cd "${SUNTRANS_HOME}"
 
 #build the classpath
 CLASSPATH="TransEditor.jar:i18n"
 for j in classes/*.jar ; do
 	CLASSPATH="$CLASSPATH:$j"				  
 done
+
 export CLASSPATH
 
 LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${SUNTRANS_HOME}/spellchecker/lib
 export LD_LIBRARY_PATH
 
-# adding a debug option here : I've turned this off at the moment,
-# but is handy if you know what you're doing and have the code
-# and a java debug facility
-xDEBUG="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=58000"
-
-JAVA=`which java`	
-if [ -d "$JAVA_HOME" ] ; then
-	JAVA="$JAVA_HOME"/bin/java
+if [ -x "$ED_JAVA_HOME/bin/java" ] ; then
+	ED_JAVA=${ED_JAVA_HOME}/bin/java
+else
+	ED_JAVA=`which java`	
 fi
 
-if [ -z "$JAVA" ] ; then 
+if [ -z "${ED_JAVA}" ] ; then 
 	echo "java does not seem to be on your system path. Please set JAVA_HOME to point to instllation of java"
 	exit 0
 fi
 
-LOGGING=-Djava.util.logging.config.file=logging.properties
 
-echo "Using java: $JAVA"
-echo "Installation direcotry: $INSTALL_PATH"
-echo "Classpath: $CLASSPATH"
+echo "Using java: ${ED_JAVA}"
+`${ED_JAVA} -version`
+echo "Installation direcotry: ${SUNTRANS_HOME}"
+echo "Classpath: ${CLASSPATH}"
 
 		
-"$JAVA" -Xmx${MEMORY}M ${DEBUG} ${LOGGING} -classpath ${CLASSPATH} -Dorg.xml.sax.driver=org.apache.xerces.parsers.SAXParser -Djava.library.path="${SUNTRANS_HOME}/spellchecker/lib" -Deditor_home="${SUNTRANS_HOME}" org.jvnet.olt.editor.translation.TransEditor
+"${ED_JAVA}" -Xmx${MEMORY}M ${DEBUG} ${LOGGING} -classpath ${CLASSPATH} -Dorg.xml.sax.driver=org.apache.xerces.parsers.SAXParser -Djava.library.path="${SUNTRANS_HOME}/spellchecker/lib" -Deditor_home="${SUNTRANS_HOME}" org.jvnet.olt.editor.translation.TransEditor
