7956d30287a449aaf7f1239758875f2815b5e5d6
andreschnabel <andreschnabel@e2a50384-0d36-e646-c335-9dcfbe33cb62>
2009-12-05 11:31:30 +0000

    descritpiton: fix classpath in windows starter batch fixes issue: 138

--- src/transeditor/installer/translation.bat
+++ src/transeditor/installer/translation.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set OTECLASSPATH=TransEditor.jar;i18n;%UserProfile%\.xliffeditor;classes\dom4j-161.jar;classes\fuzzytm.jar;classes\swing-layout-1.0.1.jar;classes\xerces2.jar;\classes\XliffBackConverter.jar;\classes\xmlParserAPIs.jar
+set OTECLASSPATH=TransEditor.jar;i18n;%UserProfile%\.xliffeditor;classes\dom4j-161.jar;classes\fuzzytm.jar;classes\swing-layout-1.0.1.jar;classes\xerces2.jar;classes\XliffBackConverter.jar;classes\xmlParserAPIs.jar
 
 java -Xmx512M -classpath "%OTECLASSPATH%" -Dorg.xml.sax.driver=org.apache.xerces.parsers.SAXParser org.jvnet.olt.editor.translation.TransEditor
 
