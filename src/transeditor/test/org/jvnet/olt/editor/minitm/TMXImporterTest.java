06a8e30ef2aed5a7466d6b234d8764059090d576
andreschnabel <andreschnabel@e2a50384-0d36-e646-c335-9dcfbe33cb62>
2010-03-30 17:00:56 +0000

    description: 	move editor's unit-tests to separate source path (src/transeditor/test) 	start to activate unit-tests again issue: -

---
+++ src/transeditor/test/org/jvnet/olt/editor/minitm/TMXImporterTest.java
@@ -0,0 +1,84 @@
+/*
+ * TMXImporterTest.java
+ *
+ * Created on September 12, 2005, 5:09 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package org.jvnet.olt.editor.minitm;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.InputStream;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.sax.SAXSource;
+import javax.xml.transform.stream.StreamResult;
+import junit.framework.TestCase;
+import org.jvnet.olt.util.TestSupport;
+import org.jvnet.olt.xliff.XLIFFEntityResolver;
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.XMLReaderFactory;
+/**
+ *
+ * @author boris
+ */
+public class TMXImporterTest extends TestCase {
+
+    /** Creates a new instance of TMXImporterTest */
+    public TMXImporterTest() {
+    }
+
+    //High level view if the test passes at all...
+    public void testSanity() throws Exception{
+        File mtmFile = TestSupport.makeFile("tmximport","1.mtm");
+        File tmxFile = TestSupport.getResourceAsFile("tmximport/1.tmx");
+
+        TMXImporter imp = new TMXImporter();
+        imp.setSrcLang("en-US");
+        imp.setTgtLang("zh-CN");
+        imp.setSrcLangShort("US");
+        imp.setTgtLangShort("ZH");
+        imp.setTranslatorId("abcdef");
+
+        imp.convertTMX2MTM(tmxFile,mtmFile);
+
+	TestSupport.catFile(mtmFile);
+
+        assertTrue(TestSupport.findLine(mtmFile, "<minitm name=\"temp\" srclang=\"US\" tgtlang=\"ZH\">", true));
+        assertTrue(TestSupport.findLine(mtmFile, "<source>ABCDEF</source>", true));
+        assertTrue(TestSupport.findLine(mtmFile, "<translation>012345</translation>", true));
+        assertTrue(TestSupport.findLine(mtmFile, "<translatorId>abcdef</translatorId>", true));
+    }
+
+    public void testRawTransform() throws Exception{
+	File tmxFile = TestSupport.getResourceAsFile("tmximport/2.tmx");
+	File outFile = TestSupport.makeFile("tmximport","rawOut.xml");
+
+	InputStream is = TMXImporter.class.getClassLoader().getResourceAsStream("xsl/tmx2mtm.xsl");
+
+	/*SAXParserFactory pf = SAXParserFactory.newInstance();
+	pf.setValidating(true);
+	pf.setNamespaceAware(true);
+	 */
+	XMLReader reader = XMLReaderFactory.createXMLReader();
+	reader.setEntityResolver(XLIFFEntityResolver.instance());
+
+	TransformerFactory tf = TransformerFactory.newInstance();
+	Transformer tr = tf.newTransformer(new SAXSource(new InputSource(is)));
+	tr.setParameter("srcLang","en-US");
+	tr.setParameter("tgtLang","zh-CN");
+	tr.setParameter("srcLangShort","US");
+	tr.setParameter("tgtLangShort","ZH");
+
+
+	tr.transform(new SAXSource(reader,new InputSource(new FileReader(tmxFile))),new StreamResult(new FileWriter(outFile)));
+
+
+    }
+
+}
---
+++ src/transeditor/test/org/jvnet/olt/editor/translation/OpenFileFiltersTest.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright  2005 Sun Microsystems, Inc.
+ * All rights reserved Use is subject to license terms.
+ *
+ */
+package org.jvnet.olt.editor.translation;
+
+import java.io.File;
+
+import javax.swing.filechooser.FileFilter;
+
+import junit.framework.TestCase;
+
+
+/*
+ * OpenFileFiltersTest.java
+ *
+ * Created on March 22, 2005, 3:03 PM
+ */
+
+/**
+ *
+ * @author boris
+ */
+public class OpenFileFiltersTest extends TestCase {
+    File correctFile1 = new File("/a/b/c/x.xlf");
+    File correctFile2 = new File("/a/b/c/y.xliff");
+    File correctFile3 = new File("/a/b/c/z.xlz");
+
+    File dir = new File("/a/b/c/");
+    File noExt1 = new File("/a/b/c/c");
+    File noExt2 = new File("/a/b/c/c.");
+
+    /** Creates a new instance of OpenFileFiltersTest */
+    public OpenFileFiltersTest() {
+    }
+
+    public void testXLFFilter() throws Exception {
+        FileFilter f = OpenFileFilters.XLF_FILTER;
+
+        assertFalse(f.accept(null));
+
+        //accept files and directories;
+        assertTrue(f.accept(new File(".")));
+        assertTrue(f.accept(new File("x.xlf")));
+        assertTrue(f.accept(new File("/ab/c/x.xlf")));
+        assertTrue(f.accept(new File("/ab/c/x.xliff")));
+    }
+
+    public void testXLZFilter() throws Exception {
+        FileFilter f = OpenFileFilters.XLZ_FILTER;
+
+        assertFalse(f.accept(null));
+
+        //accept files and directories;
+        assertTrue(f.accept(new File(".")));
+        assertTrue(f.accept(new File("x.xlz")));
+        assertTrue(f.accept(new File("/ab/c/x.xlz")));
+    }
+
+
+    public void testGetExtension() throws Exception {
+
+        assertEquals("xlf",OpenFileFilters.getExtension(correctFile1));
+        assertEquals("xliff",OpenFileFilters.getExtension(correctFile2));
+        assertEquals("xlz",OpenFileFilters.getExtension(correctFile3));
+
+        assertNull(OpenFileFilters.getExtension(dir));
+        assertNull(OpenFileFilters.getExtension(noExt1));
+        assertNull(OpenFileFilters.getExtension(noExt2));
+    }
+
+    public void testGetKnownExtensions() throws Exception {
+        assertTrue(OpenFileFilters.isFileNameXLF(correctFile1));
+        assertTrue(OpenFileFilters.isFileNameXLF(correctFile2));
+
+        assertFalse(OpenFileFilters.isFileNameXLF(dir));
+
+
+        assertTrue(OpenFileFilters.isFileNameXLZ(correctFile3));
+        assertFalse(OpenFileFilters.isFileNameXLZ(dir));
+    }
+}
