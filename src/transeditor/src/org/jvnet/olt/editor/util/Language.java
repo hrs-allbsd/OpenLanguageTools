9581b58bff02d1a37c6b1af73e96a07406f8c257
andreschnabel <andreschnabel@e2a50384-0d36-e646-c335-9dcfbe33cb62>
2010-04-22 16:05:22 +0000

    description: Language                   - make getName() aware of changes of defalut locale                   - remove unused isEmpty()                   - clean up JavaDoc issue: -

--- src/transeditor/src/org/jvnet/olt/editor/util/Language.java
+++ src/transeditor/src/org/jvnet/olt/editor/util/Language.java
@@ -32,23 +32,30 @@ import java.util.Locale;
         
 /**
  * Class for very basic language handling
- * A Language within OLTE represents an xml language and is identified by<br>
- *  an ISO language code (ISO 639-1 or 639-2 if no 639-1 code is available)<br>
- *  an optional ISO Country code ((ISO 3166)<br>
- * Internally it is handled as a Locale. See Javadoc java.util.Locale for
+ * A Language within OLTE represents an xml language and is identified by
+ * <ul>
+ *  <li>an ISO language code (ISO 639-1 or 639-2 if no 639-1 code is available)
+ *  <li>an optional ISO Country code ((ISO 3166)
+ * </ul>
+ * Internally it is handled as a Locale. See Javadoc see java.util.Locale for
  * restrictions of the ISO 639 language code handling<br>
  * In contrast to the Locale, the String representation of a
  * Language will be it's DisplayName.
  * The CompareTo method is also based on the Locale's DisplayName.
+ * @see java.util.Locale#Locale(java.lang.String) java.util.Locale for
+ *          restrictions of the ISO 639 language code handling
  */
 public class Language implements Comparable<Language>{
     private static Bundle bundle = Bundle.getBundle(Language.class.getName());
     private Locale _locale;
     private String _DisplayName;
 
+    // used for "dynamic" localization of LanguageNames
+    private Locale _currDefaultLocale;
+
     /**
      * Creates a new Language based on a given locale
-     * @param locale: Locale to create Language from
+     * @param locale Locale to create Language from
      */
     public Language( Locale locale){
         _locale = locale;
@@ -56,7 +63,7 @@ public class Language implements Comparable<Language>{
 
     /**
      * Creates a new Language identified by an language identifier
-     * @param langCode: xml:lang or Loacle-style identifier for the language code
+     * @param langCode xml:lang or Loacle-style identifier for the language code
      */
     public Language( String langCode){
         String lang = langCode.replace('_', '-');
@@ -109,7 +116,9 @@ public class Language implements Comparable<Language>{
      * @return (localized) displayname for the language
      */
     public String getName() {
-        if ( _DisplayName == null || _DisplayName.length() == 0 ) {
+
+        if ( _DisplayName == null || _DisplayName.length() == 0 ||
+            ! Locale.getDefault().equals(_currDefaultLocale)) {
             // no DisplayName retrieved yet - get the correct name
             _DisplayName = _locale.getDisplayName();
 
@@ -174,10 +183,7 @@ public class Language implements Comparable<Language>{
 
     @Override
     public String toString() {
-        return (_locale == null) ? null :  getName();    }
-
-    public boolean isEmpty(){
-        return (_locale == null);
+        return (_locale == null) ? null :  getName();
     }
 
 }
