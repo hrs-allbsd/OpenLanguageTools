6b009d22b44fdf63e7821b7ae01f1101bc5d655b
wiesemann <wiesemann@e2a50384-0d36-e646-c335-9dcfbe33cb62>
2012-01-20 16:15:48 +0000

    Corrected Java package names in shell scripts.

--- src/filters/segmenters/create-segmenter.sh
+++ src/filters/segmenters/create-segmenter.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 echo Creating segmenter for $1
-mkdir -p ${1}_segmenter/com/sun/tlis/filters/${1}_segmenter/
-cp en_segmenter/com/sun/tlis/filters/en_segmenter/* ${1}_segmenter/com/sun/tlis/filters/${1}_segmenter/
-cd ${1}_segmenter/com/sun/tlis/filters/${1}_segmenter/
+mkdir -p ${1}_segmenter/org/jvnet/olt/filters/${1}_segmenter/
+cp en_segmenter/org/jvnet/olt/filters/en_segmenter/* ${1}_segmenter/org/jvnet/olt/filters/${1}_segmenter/
+cd ${1}_segmenter/org/jvnet/olt/filters/${1}_segmenter/
 for i in *
 do
- cat $i | sed -e "s/Segmenter_en/Segmenter_${1}/g" | sed -e "s/com.sun.tlis.filters.en_segmenter/com.sun.tlis.filters.${1}_segmenter/g" > $i.new
+ cat $i | sed -e "s/Segmenter_en/Segmenter_${1}/g" | sed -e "s/org.jvnet.olt.filters.en_segmenter/org.jvnet.olt.filters.${1}_segmenter/g" > $i.new
 rm -f $i
 mv $i.new `echo $i | sed -e "s/Segmenter_en/Segmenter_${1}/g"`
 done
